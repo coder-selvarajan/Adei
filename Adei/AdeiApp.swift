2acb3f097db352bc45b828ee899e8e33c3c826ad
Selvarajan Thangavel <selvarajan2006@gmail.com>
2022-04-18 22:25:47 +0530

    Initial Commit

---
+++ Adei/AdeiApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdeiApp.swift
+//  Adei
+//
+//  Created by Selvarajan on 18/04/22.
+//
+
+import SwiftUI
+
+@main
+struct AdeiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Adei/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Adei
+//
+//  Created by Selvarajan on 18/04/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
